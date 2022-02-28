b8c91a40b160581a016b28ae4609b35dc9b6b90e
mccartneym7964 <98249410+mccartneym7964@users.noreply.github.com>
2022-02-28 14:19:59 -0500

    Update AutoFetchExes.ps1

--- AutoFetchExes.ps1
+++ AutoFetchExes.ps1
@@ -1,10 +1,10 @@
-wget "https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.30-setup.exe" #GNU Image Manipulator
-wget "https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.1.exe" # Wireshark
+wget "https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.30-setup.exe" # GNU Image Manipulator
+wget "https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.2.exe" # Wireshark
 wget "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=win64&lang=en-US" # FireFox ESR
 wget "https://download.virtualbox.org/virtualbox/6.1.32/VirtualBox-6.1.32-149290-Win.exe" # VirtualBox
-wget "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.1/npp.8.2.1.Installer.x64.exe" # Notepad ++
+wget "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.3.2/npp.8.3.2.Installer.x64.exe" # Notepad++
 wget "https://www.7-zip.org/a/7z2107-x64.exe" # 7-Zip
-wget "https://download.documentfoundation.org/libreoffice/stable/7.1.8/win/x86_64/LibreOffice_7.1.8_Win_x64.msi" # LibreOffice
+wget "https://www.libreoffice.org/donate/dl/win-x86_64/7.2.5/en-US/LibreOffice_7.2.5_Win_x64.msi" # LibreOffice
 wget "https://plug-mirror.rcac.purdue.edu/vlc/vlc/3.0.16/win64/vlc-3.0.16-win64.exe" # VLC
-wget "https://redirector.gvt1.com/edgedl/android/studio/install/2021.1.1.20/android-studio-2021.1.1.20-windows.exe" # Android Studio
+wget "https://redirector.gvt1.com/edgedl/android/studio/install/2021.1.1.22/android-studio-2021.1.1.22-windows.exe" # Android Studio
 wget "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" # Steam
