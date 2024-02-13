69af5319a3b8245452fd2f387651dddef6d35966
mccartneyms0602 <104528858+mccartneyms0602@users.noreply.github.com>
2024-02-12 21:02:27 -0500

    Update AutoFetchExes.ps1

--- AutoFetchExes.ps1
+++ AutoFetchExes.ps1
@@ -1,11 +1,11 @@
-wget "https://download.gimp.org/gimp/v2.10/windows/gimp-2.10.32-setup-1.exe" # GNU Image Manipulator
-wget "https://2.na.dl.wireshark.org/win64/Wireshark-win64-4.0.2.exe" # Wireshark
+wget "https://download.gimp.org/gimp/v2.10/windows/gimp-2.10.36-setup.exe" # GNU Image Manipulator
+wget "https://2.na.dl.wireshark.org/win64/Wireshark-4.2.2-x64.exe" # Wireshark
 wget "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=win64&lang=en-US" # FireFox ESR
-wget "https://download.virtualbox.org/virtualbox/7.0.4/VirtualBox-7.0.0-153978-Win.exe" # VirtualBox
-wget "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.7/npp.8.4.7.Installer.x64.exe" # Notepad++
-wget "https://www.7-zip.org/a/7z2201-x64.exe" # 7-Zip
-wget "https://download.documentfoundation.org/libreoffice/stable/7.3.7/win/x86_64/LibreOffice_7.3.6_Win_x64.msi" # LibreOffice
-wget "https://ftp.osuosl.org/pub/videolan/vlc/3.0.18/win64/vlc-3.0.18-win64.exe" # VLC
-wget "https://redirector.gvt1.com/edgedl/android/studio/install/2021.3.1.17/android-studio-2021.3.1.17-windows.exe" # Android Studio
+wget "https://download.virtualbox.org/virtualbox/7.0.14/VirtualBox-7.0.14-161095-Win.exe" # VirtualBox
+wget "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.2/npp.8.6.2.Installer.x64.exe" # Notepad++
+wget "https://7-zip.org/a/7z2301-x64.exe" # 7-Zip
+wget "https://www.libreoffice.org/donate/dl/win-x86_64/7.6.4/en-US/LibreOffice_7.6.4_Win_x86-64.msi" # LibreOffice
+wget "https://ftp.osuosl.org/pub/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.exe" # VLC
+wget "https://redirector.gvt1.com/edgedl/android/studio/install/2023.1.1.28/android-studio-2023.1.1.28-windows.exe" # Android Studio
 wget "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" # Steam
-wget "https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.5.0_lt20_qt6_x64_setup.exe" # qbittorrent
+wget "https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.6.3_lt20_qt6_x64_setup.exe" # qbittorrent
